92cee5178e1decdbcb95e42b27fe5660dc3cdaee
engrbugs <engrbugs@gmail.com>
2021-11-28 22:10:46 -0800

    2021-11-28-2204

--- app/src/main/java/com/example/hw1/MainActivity.kt
+++ app/src/main/java/com/example/hw1/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.hw1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            var tv = findViewById<View>(R.id.hwText) as TextView
+            tv.text = "Volume down pressed."
+        }
+        return true
     }
 }
